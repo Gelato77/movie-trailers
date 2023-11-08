9f9410400d66fd8549073900405dec8fbda4d944
Gelato77 <136853140+Gelato77@users.noreply.github.com>
2023-11-08 11:54:07 -0500

    Add Java API with Spring Boot

---
+++ MovieAPI/JavaTest/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+
+    try {
+        File file = new File("docname");
+    }
+    catch{
+        FileNotFoundException(){
+            System.out.println("File not there");
+        }
+
+    }
+
+
+}
